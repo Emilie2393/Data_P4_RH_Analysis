5b31258cdbf0e0ad6d65adf2e1d23317cee3c1ad
Emilie2393 <emilie.rich4rd@gmail.com>
2026-01-21 17:47:41 +0100

    analysis

--- files_cleaning.py
+++ files_cleaning.py
@@ -1,9 +1,25 @@
-
+import pandas as pd
 
 class FilesCleaning():
 
     def __init__(self):
-        self.
+        self.extrait_eval_df = pd.read_csv("./raw_data/extrait_eval.csv")
+        self.extrait_sirh_df = pd.read_csv("./raw_data/extrait_sirh.csv")
+        self.extrait_sondage_df = pd.read_csv("./raw_data/extrait_sondage.csv")
         
 
     def doc_analysis(self):
+
+        # On regarde comment un batiment est défini dans ce jeu de données 
+        print("------\n", self.extrait_eval_df.head())
+        print("------\n", self.extrait_sirh_df.head())
+        print("------\n", self.extrait_sondage_df.head())
+
+        # On regarde le nombre de valeurs manquantes par colonne ainsi que leur type 
+        print("------\n", self.extrait_eval_df.info())
+        print("------\n", self.extrait_sirh_df.info())
+        print("------\n", self.extrait_sondage_df.info())
+
+run = FilesCleaning()
+run.doc_analysis()
+        
