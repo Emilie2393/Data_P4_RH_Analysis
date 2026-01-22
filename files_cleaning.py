8ddd3cec970c32be112e26238057752ab02010d4
emilie <emilie.rich4rd@gmail.com>
2026-01-22 07:42:38 +0100

    analysis

--- files_cleaning.py
+++ files_cleaning.py
@@ -6,19 +6,41 @@ class FilesCleaning():
         self.extrait_eval_df = pd.read_csv("./raw_data/extrait_eval.csv")
         self.extrait_sirh_df = pd.read_csv("./raw_data/extrait_sirh.csv")
         self.extrait_sondage_df = pd.read_csv("./raw_data/extrait_sondage.csv")
+        self.df = None
         
 
     def doc_analysis(self):
 
-        # On regarde comment un batiment est défini dans ce jeu de données 
-        print("------\n", self.extrait_eval_df.head())
-        print("------\n", self.extrait_sirh_df.head())
-        print("------\n", self.extrait_sondage_df.head())
+        self.df = {
+            "eval": self.extrait_eval_df,
+            "sirh": self.extrait_sirh_df,
+            "sondage": self.extrait_sondage_df,
+        }
 
-        # On regarde le nombre de valeurs manquantes par colonne ainsi que leur type 
-        print("------\n", self.extrait_eval_df.info())
-        print("------\n", self.extrait_sirh_df.info())
-        print("------\n", self.extrait_sondage_df.info())
+        all_csv = {}
+
+        for name, df in self.df.items():
+            results = {}
+            for col in df.columns:
+                # Calcul des pourcentages de chaque valeur
+                column = df[col].value_counts(dropna=False, normalize=True).round(2).reset_index()
+                column.columns = ["valeur", "pourcentage (%)"]
+
+                # Ajouter type et % null
+                column["type"] = str(df[col].dtype)
+                column["null_%"] = round(df[col].isna().mean() * 100, 2)
+
+                results[col] = column
+
+            all_csv[name] = results
+
+        # Impression simple et propre
+        for file, res in all_csv.items():
+            print(f"\n----- Fichier : {file} -----")
+            for col, dist in res.items():
+                print(f"\nColonne: {col} - Type: {dist['type'].iloc[0]} - % null: {dist['null_%'].iloc[0]}")
+                print(dist[["valeur", "pourcentage (%)"]].to_string(index=False))
+
 
 run = FilesCleaning()
 run.doc_analysis()
