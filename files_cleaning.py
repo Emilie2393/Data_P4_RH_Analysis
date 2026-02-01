9904decc66ac0bd2b9fe7abbcd6fc3e13a98c3a6
Emilie2393 <emilie.rich4rd@gmail.com>
2026-02-01 22:01:07 +0100

    classification models

--- files_cleaning.py
+++ files_cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
@@ -8,8 +9,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score, precision_recall_curve
 
 class FilesCleaning():
 
@@ -145,11 +146,12 @@ class FilesCleaning():
         # plt.savefig("Graph/spearman_corr.png", dpi=500, bbox_inches="tight")
 
         sns.pairplot(
-        self.df[["annees_dans_l_entreprise", "annees_dans_le_poste_actuel", "annes_sous_responsable_actuel"]],
-        diag_kind="hist",
-        plot_kws={"alpha": 0.6, "s": 30})
+            self.df[["annees_dans_l_entreprise", "annees_dans_le_poste_actuel", "annes_sous_responsable_actuel"]],
+            diag_kind="hist",
+            plot_kws={"alpha": 0.6, "s": 30})
 
         plt.savefig(f"Graph/relation_pairplot.png", dpi=500, bbox_inches="tight")
+        plt.close()
 
         self.df = self.df.drop(columns=["niveau_hierarchique_poste", "annees_dans_le_poste_actuel", "annes_sous_responsable_actuel"])
 
@@ -208,20 +210,162 @@ class FilesCleaning():
                 print(f"\n--- {dataset} ---")
                 cm = confusion_matrix(y_true, y_pred)
                 print("Matrice de confusion :\n", cm)
-                print("\nClassification report :\n", classification_report(y_true, y_pred, digits=3))
+                print("\nClassification report :\n", classification_report(y_true, y_pred, digits=3, zero_division=0))
                 print(f"Precision: {precision_score(y_true, y_pred):.3f} | Recall: {recall_score(y_true, y_pred):.3f}")
-                sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-                plt.xlabel("Prédit")
-                plt.ylabel("Réel")
-                plt.title(f"{name} - {dataset}")
-                plt.show()
+                if "Dummy" not in name:
+                    plt.figure(figsize=(5, 4)) 
+                    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+                    plt.xlabel("Prédit")
+                    plt.ylabel("Réel")
+                    plt.title(f"{name} - {dataset}")
+                    plt.savefig(f"Graph/{name}_{dataset}_confusion_matrix.png", dpi=500, bbox_inches="tight")
+                    plt.close()
 
         for name, model in models.items():
             evaluate_model(name, model, X_train, y_train, X_test, y_test)
+        
+    def classification_models(self, n_estimators, class_weight=None):
+
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            self.X,
+            self.y,
+            test_size=0.2,
+            random_state=42,
+            stratify=self.y
+        )
+
+        scoring = {
+                    "precision": "precision",
+                    "recall": "recall",
+                    "f1": "f1"
+                }
+ 
+        pipe = Pipeline([
+                    ("preprocessing", self.preprocess),
+                    ("model", RandomForestClassifier(n_estimators=n_estimators, class_weight=class_weight, max_features='sqrt', random_state=42))
+                ])
+
+        cv_results = cross_validate(
+                    estimator=pipe,
+                    X=X_train,
+                    y=y_train,
+                    cv=5,
+                    scoring=scoring,
+                    return_train_score=True
+                )
+
+        metrics = ["precision", "recall", "f1"]
+        types = ["train", "test"]
+
+        rows = []
+        for metric in metrics:
+            for t in types:
+                values = cv_results[f"{t}_{metric}"]
+                rows.append({
+                    "metric": metric,
+                    "type": t,
+                    "mean": values.mean(),
+                    "std": values.std()
+                })
+
+        df_summary = pd.DataFrame(rows)
+        print(df_summary)
+
+        # Courbe précision - rappel pour déterminer le bon seuil
+        pipe.fit(X_train, y_train)
+        y_proba = pipe.predict_proba(X_test)[:, 1]
+        precision, recall, thresholds = precision_recall_curve(y_test, y_proba)
+        plt.figure(figsize=(8, 6))
+        plt.plot(recall, precision, marker='.')
+        plt.xlabel("Recall - taux de réussite total de prédiction")
+        plt.ylabel("Precision - taux de réalité des prédictions positives")
+        plt.title("Courbe Précision–Recall")
+        plt.grid(True)
+        plt.show()
+
+        target_recall = 0.80
+        min_precision = 0.40
+
+        valid = precision[:-1] >= min_precision
+
+        if valid.sum() == 0:
+            raise ValueError("Aucun seuil ne respecte la précision minimale")
+
+        filtered_recalls = recall[:-1][valid]
+        filtered_thresholds = thresholds[valid]
+
+        idx = np.argmin(np.abs(filtered_recalls - target_recall))
+        threshold = filtered_thresholds[idx]
+
+        print(f"Seuil choisi : {threshold:.3f}")
+        print(f"Recall obtenu : {filtered_recalls[idx]:.3f}")
+        print(f"Precision associée : {precision[:-1][valid][idx]:.3f}")
+
+        y_pred = (y_proba >= threshold).astype(int)
+
+        df_plot = pd.DataFrame({
+            "y_true": y_test.values,
+            "y_proba": y_proba,
+            "y_pred": y_pred
+        })
+
+        def prediction_type(row):
+            if row.y_true == 1 and row.y_pred == 1:
+                return "True Positive"
+            elif row.y_true == 0 and row.y_pred == 1:
+                return "False Positive"
+            elif row.y_true == 0 and row.y_pred == 0:
+                return "True Negative"
+            else:
+                return "False Negative"
+
+        df_plot["prediction_type"] = df_plot.apply(prediction_type, axis=1)
+
+        g = sns.displot(
+            data=df_plot,
+            x="y_proba",
+            hue="prediction_type",
+            kind="kde",
+            common_norm=False
+        )
+        g.set_axis_labels(
+            "Probabilité estimée de départ du salarié",
+            "Proportion de salariés"
+        )
+
+        for ax in g.axes.flat:
+            ax.axvline(threshold, color="black", linestyle="--")
+
+        g.fig.suptitle("Densité des probabilités par type de prédiction", y=1.02)
+
+        plt.show()
+    
+    def classification_test(self):
+
+        self.classification_models(200)
+
+        # self.X["ratio_distance_salaire"] = self.df["distance_domicile_travail"].astype(float) / self.df["revenu_mensuel"].astype(float)
+        self.X["degradation_satisfaction"] = (
+            self.df["note_evaluation_precedente"] - self.df["note_evaluation_actuelle"]
+        ).clip(lower=0)
+
+        self.X["amelioration_satisfaction"] = (
+            self.df["note_evaluation_actuelle"] - self.df["note_evaluation_precedente"]
+        ).clip(lower=0)
+
+        self.classification_models(200)
+
+
+
+
+
+
 
 
 run = FilesCleaning()
 run.doc_analysis()
 run.data_cleaning()
 run.first_modelisation()
+run.classification_test()
         
